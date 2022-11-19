80f06097f6c9b54e4b0cb4102bf4daa81ade3fe1
Alesia <alesya.r.03@gmail.com>
2022-11-19 14:09:39 +0300

    Adding new classes

---
+++ src/by/bsuir/wt/lab2/entity/enums/Color.java
@@ -0,0 +1,11 @@
+package by.bsuir.wt.lab2.entity.enums;
+
+
+public enum Color {
+    BLUE,
+    RED,
+    GREEN,
+    BLACK,
+    WHITE,
+    GREY
+}
---
+++ src/by/bsuir/wt/lab2/entity/enums/OS.java
@@ -0,0 +1,8 @@
+package by.bsuir.wt.lab2.entity.enums;
+
+public enum OS {
+    MACOS,
+    LINUX,
+    WINDOWS
+}
+
